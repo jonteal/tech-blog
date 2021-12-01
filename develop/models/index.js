7ecc131fb02fdd367fef37fdbe5127fc5466bce4
jonjackson <87287862+jonteal@users.noreply.github.com>
2021-11-30 23:43:41 -0600

    index.js in models has associations integrated

--- develop/models/index.js
+++ develop/models/index.js
@@ -2,14 +2,14 @@ const User = require('./User');
 const Comment = require('./Comment');
 const Post = require('./Post');
 
-User.hasMany(Comment, {
-    foreignKey: 'user_id',
-    onDelete: 'CASCADE'
-});
+User.hasMany(Comment, { foreignKey: 'user_id' });
+Comment.belongsTo(User, { foreignKey: 'user_id' });
 
-Comment.belongsTo(User, {
-    foreignKey: 'user_id'
-});
+User.hasMany(Post, { foreignKey: 'user_id' });
+Post.belongsTo(User, { foreignKey: 'user_id' });
+
+Post.hasMany(Comment, { foreignKey: 'post_id' });
+Comment.belongsTo(Post, { foreignKey: 'post_id' });
 
 
 
