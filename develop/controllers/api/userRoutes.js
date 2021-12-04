09dec8f51c73fdc86e8899e65bbe57abaec280f9
jonjackson <87287862+jonteal@users.noreply.github.com>
2021-12-03 23:55:29 -0600

    API userRoutes built

--- develop/controllers/api/userRoutes.js
+++ develop/controllers/api/userRoutes.js
@@ -1,25 +1,68 @@
 const router = require('express').Router();
 const { User } = require('../../models');
+const withAuth = require('../../utils/auth');
 
-router.post('/', async (req, res) => {
+
+router.post('/logout', withAuth, (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
+
+router.post('/login', async (req, res) => {
     try {
-        const userData = await User.create(req.body);
+        const userData = await User.findOne({ where: { email: req.body.email } });
 
-        req.session.save(() => {
-            req.session.user_id = userData.id;
+        if (!userData) {
+            res 
+                .status(400)
+                .json({ message: 'Incorrect email or password, please try again' });
+            return;
+        }
+
+        const validPassword = await userData.checkPassword(req.body.password);
+
+        if (!validPassword) {
+            res 
+                .status(400)
+                .json({ message: 'Incorrect email or password, please try again' });
+            return;
+        }
+
+        res.session.save(() => {
+            req.session.user_id = userData.isSoftDeleted;
             req.session.logged_in = true;
 
-            res.status(200).json(userData);
+            res.json({ user: userData, message: 'You are now logged in!' });
         });
+
     } catch (err) {
         res.status(400).json(err);
     }
 });
 
 
+router.post('/', async (req, res) => {
+    try {
+        const userData = await User.create({
+            user: req.body.user,
+            email: req.body.email,
+            password: req.body.password,
+        });
 
-
-
-
+        req.session.save(() => {
+            req.session.loggedIn = true;
+            req.session.loggedUser = userData.id;
+            res.status(200).json(userData);
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
 
 module.exports = router;
