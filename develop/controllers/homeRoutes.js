e6cc2f40a48765473379c9712cf9e5f497cae41a
jonjackson <87287862+jonteal@users.noreply.github.com>
2021-12-05 17:46:15 -0600

    homeroutes done...I think

--- develop/controllers/homeRoutes.js
+++ develop/controllers/homeRoutes.js
@@ -15,10 +15,11 @@ router.get('/', async (req, res) => {
 
         const posts = postData.map((post) => post.get({ plain: true }));
 
-        res.render('homepage', {
-            projects,
-            logged_in: req.session.logged_in
-        });
+        if (req.session.loggedIn) {
+            res.render('post', { posts, loggedIn: req.session.loggedIn, loggedUser: req.session.loggedUser });
+        } else {
+            res.render('post', { posts });
+        }
     }   catch (err) {
         res.status(500).json(err);
     }
@@ -29,18 +30,61 @@ router.get('/post/:id', async (req, res) => {
         const postData = await Post.findByPk(req.params.id, {
             include: [
                 {
-                    model: User,
-                    attributes: ['name'],
+                    model: Comment,
+                    attributes: ['comment', 'createdAt'],
+                    include: {
+                        model: User,
+                        attributes: ['user']
+                    }
                 },
+                {
+                    model: User,
+                    attributes: ['user']
+                }
             ],
         });
 
         const post = postData.get({ plain: true });
 
+        req.session.currentPost = req.params.id;
+
         res.render('post', {
             ...post,
-            logged_in: req.session.logged_in
+            loggedIn: req.session.loggedIn, loggedUser: req.session.loggedUser 
         });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+})
+
+router.get('/edit/"id', async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id);
+        const post = postData.get({ plain: true })
+        res.status(200).json(post);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+router.get('dashboard', withAuth, async (req, res) => {
+    try {
+        const userData = await User.findByPk(req.session.loggedUser, {
+            include: {
+                model: Post,
+            },
+            attributes: { excludes: ['password'] }
+        });
+        user = userData.get({ plain: true })
+        
+        res.render('dashboard', { user, loggedIn: req.session.loggedIn, loggedUser: req.session.loggedUser })
     } catch (err) {
         res.status(500).json(err);
     }
