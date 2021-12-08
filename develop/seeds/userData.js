160d469361d3267cf08b385b471dd929961a0010
jonjackson <87287862+jonteal@users.noreply.github.com>
2021-12-07 21:37:48 -0600

    Revamped all seed files to js files and stored the seeds in const variables

---
+++ develop/seeds/userData.js
@@ -0,0 +1,24 @@
+const { User } = require('../models');
+
+const userData = [
+  {
+    "username": "Sal",
+    "password": "password12345"
+  },
+  {
+    "username": "Lernantino",
+    "password": "password12345"
+  },
+  {
+    "username": "Amiko",
+    "password": "password12345"
+  },
+  {
+    "username": "Enrique",
+    "password": "password12345"
+  }
+];
+
+const seedUser = () => User.bulkCreate(userData);
+
+module.exports = seedUser;
